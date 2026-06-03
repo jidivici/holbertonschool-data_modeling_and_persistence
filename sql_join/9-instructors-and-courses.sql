bc4277c842935bbf68e0fc16f17d0a30a8df5727
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-03 19:45:11 +0200

    fix: uppercase ON keyword and add missing semicolon

--- sql_join/9-instructors-and-courses.sql
+++ sql_join/9-instructors-and-courses.sql
@@ -1,4 +1,4 @@
 SELECT i.name AS instructor_name, c.title AS course_title
 FROM instructors i
-LEFT JOIN courses c on c.instructor_id = i.id
-ORDER BY i.name ASC, c.title ASC
+LEFT JOIN courses c ON c.instructor_id = i.id
+ORDER BY i.name ASC, c.title ASC;
