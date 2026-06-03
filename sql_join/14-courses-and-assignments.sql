9c290511ee045513f1f343845d3406a11c7c9fe2
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-03 19:45:28 +0200

    fix: remove main. schema prefix and add missing semicolon

--- sql_join/14-courses-and-assignments.sql
+++ sql_join/14-courses-and-assignments.sql
@@ -2,5 +2,5 @@ SELECT
     c.title AS course_title,
     a.title AS assignment_title
 FROM courses c
-LEFT JOIN main.assignments a on c.id = a.course_id
-ORDER BY c.title ASC, a.title ASC
+LEFT JOIN assignments a ON c.id = a.course_id
+ORDER BY c.title ASC, a.title ASC;
