5c174ad59ee2ef155162f5ef966d8da2591f1684
Brice Fontaine <brice.fontaine.83@icloud.com>
2026-06-03 19:45:33 +0200

    fix: remove trailing blank line and add missing semicolon

--- sql_join/15-instructors-with-active-courses.sql
+++ sql_join/15-instructors-with-active-courses.sql
@@ -2,5 +2,4 @@ SELECT DISTINCT i.name AS instructor_name
 FROM instructors i
 JOIN courses c ON i.id = c.instructor_id
 JOIN registrations r ON c.id = r.course_id
-ORDER BY instructor_name ASC
-
+ORDER BY instructor_name ASC;
